7c0f0c2162d0bbf7dc74d51f5c00186e1d84c6e0
Kartik Kukreja <kartikkukreja92@gmail.com>
2025-04-08 16:52:55 +0000

    Update API root to use codespace URL and configure allowed hosts for OctoFit Tracker

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%m2d$tz+98uu2ad2xt)c*(scw6=me7n=^y#0l67ew&he-b9d*#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'legendary-space-cod-vpvr76vrqqhxggp-8000.app.github.dev']
 
 
 # Application definition
